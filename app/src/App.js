0db8d7eba13bad35550938b540930f005a781c79
Natalie <nkfelic1@asu.edu>
2025-09-27 16:09:30 -0700

    Implemented closed blind box shake animation

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,25 @@
 //import logo from './logo.svg';
+import React, { useState } from 'react';
 import ClosedBox from "./assets/ClosedBox.png"
 import './App.css';
 
 function App() {
 
+  const [isShaking, setIsShaking] = useState(false);
+
+  const handleShake = () => {
+    setIsShaking(true);
+    setTimeout(() => setIsShaking(false), 3000); // 3 seconds
+  };
+
   //keep all here
   return (
     <div className="App">
       <header className="App-header">
-        <img src={ClosedBox} className="Closed-Box" alt="Closed Blind Box" />
-        
+        <img src={ClosedBox} className={`Closed-Box ${isShaking ? "shake" : ""}`} alt="Closed Blind Box" />
 
-        <button className="App-button" onClick={() => alert('Button clicked!')}>
-        OPEN BOX
+        <button className="App-button" onClick={handleShake}>
+          OPEN BOX
         </button>
 
         <a
