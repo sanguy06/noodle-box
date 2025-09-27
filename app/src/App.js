31fd1952612991e9c8426755374c86859926bb95
Tatyana <tsacdala@asu.edu>
2025-09-27 15:47:23 -0700

    Start Page Layout complete

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import ClosedBox from "./assets/ClosedBox.png"
 import './App.css';
 
 function App() {
+
+  //keep all here
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={ClosedBox} className="Closed-Box" alt="Closed Blind Box" />
+        
+
+        <button className="App-button" onClick={() => alert('Button clicked!')}>
+        OPEN BOX
+        </button>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+
         </a>
       </header>
     </div>
