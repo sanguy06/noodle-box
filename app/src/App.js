d66f19806136a8cce7632a1c9f820223ec7e3cd1
Sara Nguyen <sara.nguyen1035@gmail.com>
2025-09-27 17:31:15 -0700

    Forget these changes. Conflict between fade-out

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,68 @@
 //import logo from './logo.svg';
+import React, { useState } from 'react';
 import ClosedBox from "./assets/ClosedBox.png"
+import RamenOpended from "./assets/RamenOpened.png"
+//import PhoClosed from "./assets/PhoClosed.png"
+//import UdonClosed from "./assets/UdonClosed.png"
 import './App.css';
-import { useNavigate } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Switch from "./pages/Switch";
+
 function App() {
 
-  const navigate = useNavigate("")
+  //shaking box function
+  const [isShaking, setIsShaking] = useState(false);
+  const [isFading, setIsFading] = useState(false);
 
-  return(
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/switch" element={<Switch />} />
-      </Routes>
+  const handleShake = () => {
+    setIsShaking(true);
+    setTimeout(() => { setIsShaking(false); setIsFading(false); setShowImages(true);} ,3000); // 3 seconds
+  };
+
+  
+
+  const [showImages, setShowImages] = useState(false);
+
+  //New Page: P1
+  if (showImages) {
+    // This is the "new page" with 3 
+   return (
+    <div className="container">
+      <div className="side left">
+        <img src={RamenOpended} alt="Left" />
+      </div>
+      <div className="side right">
+        <img src={PhoClosed} alt="Right" />
+      </div>
+      <div className="middle">
+        <img src={UdonClosed} alt="Middle" />
+      </div>
+      <button className="App-button" onClick={() => setShowImages(false)}>
+        Go Back
+      </button>
     </div>
-  )
+  );
+  }
+
+  //Main: PG0
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ClosedBox} className={`Closed-Box ${isShaking ? "shake" : ""}`} alt="Closed Blind Box" />
+
+        <button className="App-button" onClick={handleShake}>
+          OPEN BOX
+        </button>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
